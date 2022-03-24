edaa6cb55466944c62bd06fe5640226da8694b96
Abraham Ora <abraham.morial.ora@gmail.com>
2022-03-24 15:05:28 +0800

    Modify the locators class for some additional locator techniques

--- introduction/src/introduction/locators.java
+++ introduction/src/introduction/locators.java
@@ -1,15 +1,13 @@
 package introduction;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class locators {
+public class Locators {
 
 	public static void main(String[] args) throws InterruptedException {
 		
